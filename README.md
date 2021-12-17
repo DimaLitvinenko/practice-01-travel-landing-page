0cd67c43eabb0c605a0a332f626d34b2836dc849
DimaLitvinenko <dimaly.too@gmail.com>
2021-12-17 22:28:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# practice-01-travel-landing-page
+ 
---
+++ js/main.js
@@ -0,0 +1,24 @@
+(function () {
+     const header = document.querySelector('.header');
+    window.onscroll = () => {
+         if (window.pageYOffset > 200) {
+             header.classList.add('header_active');
+         } else {
+            header.classList.remove('header_active');
+         }
+    };
+}());
+
+// Burger hangler
+
+(function() {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header_nav')
+    const menuCloseItem = document.querySelector('.header_nav_close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header_nav_active');
+    });
+    menuCloseItem.addEventListener('click',() => {
+        menu.classList.remove('header_nav_active');
+    })
+}());
